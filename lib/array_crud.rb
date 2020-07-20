da5516bd13edd83bf64b3b85459a78de4c416e19
Brian Arango <brianvictus@gmail.com>
2020-07-20 23:58:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -54,8 +54,7 @@ def add_element_to_start_of_array(array, element)
   [].push(array)
 end
 
-def remove_element_from_end_of_array(array, element)
-  array = element
+def remove_element_from_end_of_array(array)
   [].pop(array)
 end
   
