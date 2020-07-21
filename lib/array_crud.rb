22a2b4c6f2642c1b9ebde59f5dedebc43feba7d8
Brian Arango <brianvictus@gmail.com>
2020-07-21 00:23:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -68,9 +68,12 @@ def retrieve_element_from_index(array, index)
 end
 
 def retrieve_first_element_from_array(array)
-  array.shift
+  #array.shift
 end
   
+def retrieve_last_element_from_array(array)
+  #array.pop
+end
   
   
   
